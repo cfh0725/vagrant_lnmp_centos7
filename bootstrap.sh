18e13f28c2f851bd1a2811ea3390598186b348fb
Jason Chen <cfh0725@gmail.com>
2016-08-14 21:42:54 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# add yum repo
-rpm -Uvh http://download.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://dl.iuscommunity.org/pub/ius/stable/CentOS/7/x86_64/ius-release-1.0-14.ius.centos7.noarch.rpm
-rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+# yum repoyum update
+yum install -y epel-release
+
+rpm -Uvh https://centos7.iuscommunity.org/ius-release.rpm
+rpm -Uvh https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 
 yum update -y
 yum upgrade -y
-yum update cache fast
 
 # nfs
-yum install nfs-utils -y
-systemctl enable nfs-server
-systemctl start nfs-server
+#yum install -y nfs-utils
+#systemctl enable nfs-server
+#systemctl start nfs-server
 
 # mariadb
-#yum install mariadb-server -y
+#yum install -y mariadb-server
 #systemctl enable mariadb
 #systemctl start mariadb
 
 # mysql
-rpm -ivh http://repo.mysql.com/mysql-community-release-el7-7.noarch.rpm
-yum install mysql-community-server -y
+rpm -ivh https://repo.mysql.com/mysql-community-release-el7-7.noarch.rpm
+yum install -y mysql-community-server
 systemctl enable mysqld
 systemctl start mysqld
 
@@ -35,7 +35,7 @@ FLUSH PRIVILEGES;
 EOF
 
 # php 5.6
-yum install php56u-cli php56u-fpm php56u-gd php56u-json php56u-mbstring php56u-mcrypt php56u-mysqlnd php56u-opcache php56u-pdo php56u-pgsql php56u-xml php56-php-pecl-zip php56u-pecl-memcache php56u-pecl-memcached -y
+yum install -y php56u-cli php56u-fpm php56u-gd php56u-json php56u-mbstring php56u-mcrypt php56u-mysqlnd php56u-opcache php56u-pdo php56u-xml php56-php-pecl-zip php56u-pecl-memcache php56u-pecl-memcached
 sed -i "s/listen = 127.0.0.1:9000/listen = 127.0.0.1:9001/" /etc/php-fpm.d/www.conf
 sed -i "s/;date.timezone =/date.timezone = Asia\/Taipei/" /etc/php.ini
 sed -i "s/memory_limit = 128M/memory_limit = 512M/" /etc/php.ini
@@ -43,7 +43,7 @@ systemctl enable php-fpm
 systemctl start php-fpm
 
 # php 7
-yum --enablerepo=remi install php70-php-cli php70-php-fpm php70-php-gd php70-php-json php70-php-mbstring php70-php-mcrypt php70-php-mysqlnd php70-php-opcache php70-php-pdo php70-php-pgsql php70-php-xml php70-php-pecl-zip -y
+yum --enablerepo=remi install -y php70-php-cli php70-php-fpm php70-php-gd php70-php-json php70-php-mbstring php70-php-mcrypt php70-php-mysqlnd php70-php-opcache php70-php-pdo php70-php-xml php70-php-pecl-zip
 sed -i "s/;date.timezone =/date.timezone = Asia\/Taipei/" /etc/opt/remi/php70/php.ini
 sed -i "s/memory_limit = 128M/memory_limit = 512M/" /etc/opt/remi/php70/php.ini
 systemctl enable php70-php-fpm
@@ -54,12 +54,12 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # redis
-yum install redis -y
+yum install -y redis
 systemctl enable redis
 systemctl start redis
 
 # nginx
-yum install nginx -y
+yum install -y nginx
 rm -rf /etc/nginx/conf.d
 ln -s /vagrant/nginx/conf.d/ /etc/nginx
 sed -i "s/sendfile[ ][ ]*on/sendfile off/" /etc/nginx/nginx.conf
@@ -67,18 +67,18 @@ systemctl enable nginx
 systemctl start nginx
 
 # vim
-yum install vim -y
+yum install -y vim
 sed -i -e "\$a\ " /etc/vimrc
 sed -i -e "\$asyntax on" /etc/vimrc
 sed -i -e "\$aset nu" /etc/vimrc
 
 # utilities
-yum install wget git htop net-tools vim -y
+yum install -y wget git htop net-tools vim
 
-# firewall
-systemctl disable firewalld
-systemctl stop firewalld
+# disable firewall
+#systemctl disable firewalld
+#systemctl stop firewalld
 
-# selinux
-sed -i 's/enforcing/disabled/g' /etc/selinux/config
-reboot
+# disable selinux
+#sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+#reboot
