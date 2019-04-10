ac5bea257ec9dbb080741d6eecd8e2f1c97fc28a
Jason Chen <cfh0725@gmail.com>
2019-04-10 12:35:08 +0800

    Update bootstrap.sh

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # firewall
-#firewall-cmd --permanent --zone=public --add-service=http
-#firewall-cmd --reload
 systemctl disable firewalld
 systemctl stop firewalld
 
@@ -10,8 +8,9 @@ systemctl stop firewalld
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/" /etc/selinux/config
 
-# add third-party yum repo
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# third party yum repository
+yum install -y epel-release
+# yum install -y centos-release-scl
 rpm -Uvh https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum update -y
 yum upgrade -y
@@ -22,12 +21,9 @@ systemctl enable redis
 systemctl start redis
 
 # mariadb
-curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
-yum install -y MariaDB-server MariaDB-client
-
+yum install -y mariadb-server mariadb-client
 systemctl enable mariadb
 systemctl start mariadb
-
 mysql -u root <<EOF
     UPDATE mysql.user SET Password=PASSWORD('show_me_the_data') WHERE User='root';
     DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
@@ -47,29 +43,19 @@ systemctl start nginx
 
 # php 7.3
 yum --enablerepo=remi,remi-php73 install -y php-cli php-fpm php-gd php-intl php-json php-mbstring php-mcrypt php-mysqlnd php-opcache php-pdo php-xml php-pecl-zip
+sed -i "s/listen = 127.0.0.1:9000/listen = 127.0.0.1:9073/" /etc/php-fpm.d/www.conf
 sed -i "s/;date.timezone =/date.timezone = Asia\/Taipei/" /etc/php.ini
 sed -i "s/memory_limit = 128M/memory_limit = 512M/" /etc/php.ini
-
 systemctl enable php-fpm
 systemctl start php-fpm
 
-# php 5.6
+## php 5.6
 yum --enablerepo=remi install -y php56-php-cli php56-php-fpm php56-php-gd php56-php-intl php56-php-json php56-php-mbstring php56-php-mcrypt php56-php-mysqlnd php56-php-opcache php56-php-pdo php56-php-xml php56-php-pecl-zip
-sed -i "s/listen = 127.0.0.1:9000/listen = 127.0.0.1:9001/" /opt/remi/php56/root/etc/php-fpm.d/www.conf
+sed -i "s/listen = 127.0.0.1:9000/listen = 127.0.0.1:9056/" /opt/remi/php56/root/etc/php-fpm.d/www.conf
 sed -i "s/;date.timezone =/date.timezone = Asia\/Taipei/" /opt/remi/php56/root/etc/php.ini
 sed -i "s/memory_limit = 128M/memory_limit = 512M/" /opt/remi/php56/root/etc/php.ini
-
 systemctl enable php56-php-fpm
 systemctl start php56-php-fpm
-
-# php 7.0
-# yum --enablerepo=remi,remi-php70 install -y php70-php-cli php70-php-fpm php70-php-gd php70-php-intl php70-php-json php70-php-mbstring php70-php-mcrypt php70-php-mysqlnd php70-php-opcache php70-php-pdo php70-php-xml php70-php-pecl-zip
-# sed -i "s/listen = 127.0.0.1:9000/listen = 127.0.0.1:9002/" /etc/opt/remi/php70/php-fpm.d/www.conf
-# sed -i "s/;date.timezone =/date.timezone = Asia\/Taipei/" /etc/opt/remi/php70/php.ini
-# sed -i "s/memory_limit = 128M/memory_limit = 512M/" /etc/opt/remi/php70/php.ini
-
-# systemctl enable php70-php-fpm
-# systemctl start php70-php-fpm
 
 # composer
 curl -sS https://getcomposer.org/installer | php
